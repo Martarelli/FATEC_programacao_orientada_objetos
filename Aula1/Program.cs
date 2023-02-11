53ff92b55f0906cf54f7552b234094bc08ed3016
Martarelli <renanmartarelli@gmail.com>
2023-02-11 10:12:07 -0300

    calculo de idade

--- Aula1/Program.cs
+++ Aula1/Program.cs
@@ -1,2 +1,10 @@
 ﻿// main
 Console.WriteLine("Hello, World!");
+
+System.Console.WriteLine("...Programa para calcular a idade...");
+
+System.Console.Write("Informe o ano de nascimento: ");
+int ano_nasc = Convert.ToInt32(Console.ReadLine());
+int idade = 2023 - ano_nasc;
+
+System.Console.WriteLine("Sua idade é " + idade + " anos.");
