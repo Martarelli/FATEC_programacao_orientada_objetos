64fa17914c608d73cd21edb5d629b1e82537a80e
Martarelli <renanmartarelli@gmail.com>
2023-02-17 21:50:24 -0300

    atributos e metodos

--- POO_noite/ClasseConta/Conta.cs
+++ POO_noite/ClasseConta/Conta.cs
@@ -7,8 +7,14 @@ namespace POO_noite.ClasseConta
 {
     public class Conta
     {
+        //ATRIBUTOS
         public int numero;
         public string titular;
         public double saldo;
+
+    //METODOS
+        public void Sacar();
+
+        public void Depositar();
     }
 }
