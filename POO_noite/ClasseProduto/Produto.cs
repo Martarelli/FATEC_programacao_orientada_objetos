9f15fdaa584d7727cd4ae975251fa294e0225d19
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-02-21 20:34:54 -0300

    classe produto

---
+++ POO_noite/ClasseProduto/Produto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClasseProduto
+{
+    public class Produto
+    {
+        public int codigo;
+        public string descricao;
+        public double preco;
+
+        public void mostrarDados(){
+            System.Console.WriteLine("CODIGO: " + codigo);
+            System.Console.WriteLine("DESCRIÇÃO: " + descricao);
+            System.Console.WriteLine("PREÇO: " + preco);
+        }
+    }
+}
