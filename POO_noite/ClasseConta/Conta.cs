b657c40987c8f32083c7ff190ff78b759fcf3a11
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-02-21 20:51:47 -0300

    metodo transferir

--- POO_noite/ClasseConta/Conta.cs
+++ POO_noite/ClasseConta/Conta.cs
@@ -22,5 +22,19 @@ namespace ClasseConta
             System.Console.WriteLine("Titular: " + titular);
             System.Console.WriteLine("Saldo: " + saldo);
         }
+
+        public void transferencia(double valor, Conta c1, Conta c2){
+            if (c1.saldo > 0 && c1.saldo - valor >= 0)
+            {
+                c1.saldo -= valor;
+                c2.saldo += valor;
+                System.Console.WriteLine("Novo saldo conta titular: " + c1.titular + " = R$"+ c1.saldo);
+                System.Console.WriteLine("Novo saldo conta titular: " + c2.titular + " = R$"+ c2.saldo);  
+            }
+            else
+            {
+                System.Console.WriteLine("Saldo insuficiente...\n");
+            }
+        }
     }
 }
