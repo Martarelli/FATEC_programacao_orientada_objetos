6fc4fe862d92fb8e22a69f9d50ccc2321d46e297
Martarelli <renanmartarelli@gmail.com>
2023-02-25 10:08:47 -0300

    cadastro de contas

--- POO_noite/ArrayConta/Program.cs
+++ POO_noite/ArrayConta/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Internal;
+using System;
+using ClasseConta;
+
+Conta[] vetConta = new Conta[3];
+
+for (int i = 0; i < vetConta.length; i++)
+{
+    vetConta[i] = new Conta();
+    System.Console.Write("Digite o número: ");
+    vetConta[i].numero = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("Digite o titular: ");
+    vetConta[i].numero = Console.ReadLine();
+    System.Console.Write("Digite o saldo: ");
+    vetConta[i].numero = Convert.ToDouble(Console.ReadLine());
+}
+
+foreach (Conta c in vetConta)
+{
+    c.MostrarAtributos();
+}
+
