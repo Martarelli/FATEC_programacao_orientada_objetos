a3135d7a0d3a82ec4eebef82e5a34940cae98a79
Martarelli <renanmartarelli@gmail.com>
2023-03-04 10:04:31 -0300

    classe produto

---
+++ POO_noite/ArrayProduto/Produto.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArrayProduto
+{
+    public class Produto
+    {
+        private int codigo;
+        private string nome;
+        private double preco;
+        public int Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        
+        public double Preco
+        {
+            get { return preco; }
+            set { preco = value; }
+        }
+        
+
+        
+    }
+}
