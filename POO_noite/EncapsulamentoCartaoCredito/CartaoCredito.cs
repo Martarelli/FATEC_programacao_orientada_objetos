7e4f0c5a8eccf699167f32899d0e96f30b27d23d
Martarelli <renanmartarelli@gmail.com>
2023-03-04 10:48:49 -0300

    exericicio cartao de credito

---
+++ POO_noite/EncapsulamentoCartaoCredito/CartaoCredito.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EncapsulamentoCartaoCredito
+{
+    public class CartaoCredito
+    {
+        //int numero
+        //string nome
+        //double saldo
+        //int anoVencimento
+        //int codigoSeguranca
+
+        //implementar os metodos de encapsulamento compacto e completo para os atributos
+        //valide o ano ser acima de 2022
+        //realize algumas instacias e teste
+    }
+}
