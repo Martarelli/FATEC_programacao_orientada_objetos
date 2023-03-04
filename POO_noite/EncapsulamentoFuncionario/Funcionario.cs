44bab3c908b77c613aa4f7f988e1a6adc8149c8d
Martarelli <renanmartarelli@gmail.com>
2023-03-03 22:01:59 -0300

    getters e setters

--- POO_noite/EncapsulamentoFuncionario/Funcionario.cs
+++ POO_noite/EncapsulamentoFuncionario/Funcionario.cs
@@ -8,8 +8,37 @@ namespace EncapsulamentoFuncionario
     public class Funcionario
     {
         private int codigo;
-        private string nome;
+        private string nome = "";
         private double salario;
+        public int Codigo
+        {
+            get { 
+                return this.codigo; 
+                }
+            set { 
+                this.codigo = value; 
+                }
+        }
+
+        public string Nome
+        {
+            get { 
+                return this.nome; 
+                }
+            set { 
+                this.nome = value; 
+                }
+        }
+
+                public double Salario
+        {
+            get { 
+                return this.salario; 
+                }
+            set { 
+                this.salario = value; 
+                }
+        }
 
         public void MostrarDados(){
             System.Console.WriteLine("\nCODIGO: " + codigo);
