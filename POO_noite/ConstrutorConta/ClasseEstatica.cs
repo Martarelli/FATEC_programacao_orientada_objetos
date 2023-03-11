aa8a222235161a5cc346da92a0ee107ec16c1bb3
Martarelli <renanmartarelli@gmail.com>
2023-03-11 10:04:42 -0300

    classe estatica

---
+++ POO_noite/ConstrutorConta/ClasseEstatica.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstrutorConta
+{
+    public static class ClasseEstatica
+    {
+        public static void MostraFrase()
+        {
+            System.Console.WriteLine("Hello World!!!");
+        }
+    }
+}
