1953d95030a43271e84ef45e1d252373ae2c7f1b
Martarelli <renanmartarelli@gmail.com>
2023-03-17 22:14:38 -0300

    atributos e construtor

--- POO_noite/AgregacaoConta/Cliente.cs
+++ POO_noite/AgregacaoConta/Cliente.cs
@@ -7,6 +7,20 @@ namespace AgregacaoConta
 {
     public class Cliente
     {
+        public string Nome { get; set; } = "";
+
+        public int Cpf { get; set; }
         
+        public string Endereco { get; set; } = "";
+
+        public Cliente()
+        {
+        }
+        public Cliente(string n, int c, string e)
+        {
+            Nome = n;
+            Cpf = c;
+            Endereco = e;
+        }
     }
 }
