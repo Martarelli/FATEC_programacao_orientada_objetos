773fcb2ee2c3c9fb84009673a1c4315be51cde90
Martarelli <renanmartarelli@gmail.com>
2023-03-18 09:58:51 -0300

    atributos e getters e setters

--- POO_noite/AgregacaoContaVetor/Cliente.cs
+++ POO_noite/AgregacaoContaVetor/Cliente.cs
@@ -7,6 +7,15 @@ namespace AgregacaoContaVetor
 {
     public class Cliente
     {
-        
+        public string Nome { get; set; }
+        public int Cpf { get; set; }
+        public int Rg { get; set; }
+
+        public Cliente(string nome, int cpf, int rg)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Rg = rg;
+        }
     }
 }
