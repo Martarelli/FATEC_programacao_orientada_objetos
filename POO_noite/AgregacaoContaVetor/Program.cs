470cbb37cc86db09de59aeac10d97a5200b0056e
Martarelli <renanmartarelli@gmail.com>
2023-03-18 10:32:28 -0300

    metodo main

--- POO_noite/AgregacaoContaVetor/Program.cs
+++ POO_noite/AgregacaoContaVetor/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AgregacaoContaVetor;
+
+Conta c1 = new Conta();
+c1.Numero = 1;
+c1.Saldo = 22334.56;
+
+Cliente cli1 = new Cliente("Josivaldo",111111,111);
+Cliente cli2 = new Cliente("Josivaldo",222222,222);
+Cliente cli3 = new Cliente("Josivaldo",333333,333);
+
+c1.vetTitular = new List<Cliente>();
+c1.vetTitular.Add(cli1);
+c1.vetTitular.Add(cli2);
+c1.vetTitular.Add(cli3);
+
+foreach(Cliente c in c1.vetTitular)
+{
+    c.MostrarCliente();
+}
