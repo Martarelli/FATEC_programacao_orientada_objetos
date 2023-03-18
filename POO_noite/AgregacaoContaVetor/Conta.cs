97a1f199a46a7b5b891d95635477b95305b4c25c
Martarelli <renanmartarelli@gmail.com>
2023-03-18 10:39:04 -0300

    classe conta

--- POO_noite/AgregacaoContaVetor/Conta.cs
+++ POO_noite/AgregacaoContaVetor/Conta.cs
@@ -9,7 +9,7 @@ namespace AgregacaoContaVetor
     {
         public int Numero { get; set; }
         public double Saldo { get; set; }
-        public List<Cliente> vetTitular { get; set; }
+        public List<Cliente> vetTitular { get; set; } 
         public void MostrarConta()
         {
             System.Console.WriteLine($"Numero:{Numero}\tSaldo: {Saldo}");
