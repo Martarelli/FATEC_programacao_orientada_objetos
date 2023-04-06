b52741b52da7eff0cc1719599d7ca5f0e32e8296
Martarelli <renanmartarelli@gmail.com>
2023-04-06 10:13:31 -0300

    metodo exibir contas correntes

--- POO_noite/1º Bimestre/ExercicioComposicao/Program.cs
+++ POO_noite/1º Bimestre/ExercicioComposicao/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExercicioComposicao;
+
+Banco b1 = new Banco();
+b1.AbrirConta(2000.00, 1000);
+b1.AbrirConta(34567.98, 100);
+b1.AbrirConta(3321.88, 40);
+b1.AbrirConta(2234.09, 155);
+
