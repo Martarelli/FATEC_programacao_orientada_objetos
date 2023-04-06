417a27a5f285adf25238da787b6c461d9a515db0
Martarelli <renanmartarelli@gmail.com>
2023-04-06 09:15:28 -0300

    corrigindo

--- POO_noite/1º Bimestre/ConstrutorFuncionario/Program.cs
+++ POO_noite/1º Bimestre/ConstrutorFuncionario/Program.cs
@@ -1,13 +1,1 @@
-﻿using ExercicioAgregacao;
-
-Produto p1 = new Produto(1299.99f, 2);
-p1.Vendido();
-
-Comprador c1 = new Comprador(2500f);
-c1.Compra(p1);
-
-Vendedor v1 = new Vendedor(10f);
-v1.Vende(p1);
-
-Venda venda1 = new Venda(p1, v1, c1);
-venda1.ConcretizarVenda();
+﻿
