4f1ef20d04dd0b9108584174bcfedd072c94bbb6
Martarelli <renanmartarelli@gmail.com>
2023-04-06 09:53:27 -0300

    atributos e construtor

--- POO_noite/1º Bimestre/ExercicioComposicao/Banco.cs
+++ POO_noite/1º Bimestre/ExercicioComposicao/Banco.cs
@@ -7,6 +7,17 @@ namespace ExercicioComposicao
 {
     public class Banco
     {
-        
+        public List<Poupanca> Poupancas { get; set; }
+        public List<ContaCorrente> ContasCorrentes { get; set; }
+
+        public Banco()
+        {
+            
+        }
+        public Banco(List<Poupanca> p, List<ContaCorrente> c)
+        {
+            Poupancas = p;
+            ContasCorrentes = c;
+        }
     }
 }
