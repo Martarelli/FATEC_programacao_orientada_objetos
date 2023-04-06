9798580cef53c2d9eb8623db411b35aa5c5726ef
Martarelli <renanmartarelli@gmail.com>
2023-04-06 10:20:57 -0300

    metodo main composição

--- POO_noite/1º Bimestre/ExercicioComposicao/Program.cs
+++ POO_noite/1º Bimestre/ExercicioComposicao/Program.cs
@@ -5,4 +5,11 @@ b1.AbrirConta(2000.00, 1000);
 b1.AbrirConta(34567.98, 100);
 b1.AbrirConta(3321.88, 40);
 b1.AbrirConta(2234.09, 155);
+b1.AbrirPoupanca(234345.22);
+b1.AbrirPoupanca(3456.55);
+b1.AbrirPoupanca(10000.00);
 
+b1.ExibirContas();
+b1.ExibirPoupancas();
+
+b1.AplicarRendimentoPoupanca();
