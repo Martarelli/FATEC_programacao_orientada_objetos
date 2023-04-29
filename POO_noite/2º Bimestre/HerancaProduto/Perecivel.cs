d6fc59ea5602853d58fde269e4612d0a9196c310
Martarelli <renanmartarelli@gmail.com>
2023-04-29 10:57:16 -0300

    exercicio herança produto

---
+++ POO_noite/2º Bimestre/HerancaProduto/Perecivel.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HerancaProduto
+{
+    public class Perecivel : Produto
+    {
+        protected string dtValidade;
+        public string DtValidade
+        {
+            get { return dtValidade; }
+            set { dtValidade = value; }
+        }
+
+        private string dtFabricacao;
+        public string Dtfabricacao
+        {
+            get { return dtFabricacao; }
+            set { dtFabricacao = value; }
+        }
+        private int lote;
+        public int Lote
+        {
+            get { return lote; }
+            set { lote = value; }
+        }
+        
+        public Perecivel(){
+
+        }
+        public Perecivel(int codigo, string nome, double preco, string dtValidade, string dtFabricacao, int lote) : base(codigo, nome, preco)
+        {
+            DtValidade = dtValidade;
+            Dtfabricacao = dtFabricacao;
+            Lote = lote;
+        }
+
+        public void Mostrar()
+        {
+            base.Mostrar();
+            System.Console.WriteLine($"Data Validade: {DtValidade}\tData Fabricação: {Dtfabricacao}\tLote: {Lote}");
+        }
+        
+    }
+}
---
+++ POO_noite/2º Bimestre/HerancaProduto/Program.cs
@@ -0,0 +1,7 @@
+﻿using HerancaProduto;
+
+Produto p = new Produto(1, "Notebook", 9999.99);
+p.Mostrar();
+
+Perecivel pp = new Perecivel(2, "Danone", 2.56, "11-11-1111", "22-22-2222", 134);
+pp.Mostrar();
