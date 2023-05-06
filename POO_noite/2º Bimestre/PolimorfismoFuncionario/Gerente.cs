143d56c60ba80e49f93cd2cb16e645e2e28d0d08
Martarelli <renanmartarelli@gmail.com>
2023-05-05 21:53:41 -0300

    exercicio resolvido

---
+++ POO_noite/2º Bimestre/PolimorfismoFuncionario/Gerente.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PolimorfismoFuncionario
+{
+    public class Gerente : Funcionario
+    {
+        public Gerente(int c, string n, double s) : base(c, n, s)
+        {
+
+        }
+        public override double CalcularBonificacao()
+        {
+            return Salario * 15 / 100;
+        }
+    }
+}
--- POO_noite/2º Bimestre/PolimorfismoFuncionario/Program.cs
+++ POO_noite/2º Bimestre/PolimorfismoFuncionario/Program.cs
@@ -5,3 +5,9 @@ System.Console.WriteLine($"{f.Nome} - Bonificação {f.CalcularBonificacao():C}"
 
 Secretario s = new Secretario(1, "Gabi", 1000, 11);
 System.Console.WriteLine($"{s.Nome} - Bonificação {s.CalcularBonificacao():C}");
+
+Gerente g = new Gerente(1, "Fulano", 2000);
+System.Console.WriteLine($"{g.Nome} - Bonificação {g.CalcularBonificacao():C}");
+
+Diretor d = new Diretor(1, "Fulano", 2000);
+System.Console.WriteLine($"{d.Nome} - Bonificação {d.CalcularBonificacao():C}");
