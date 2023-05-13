2fca5a47bb4c279d03f5fd081ca979b117e8e567
Martarelli <renanmartarelli@gmail.com>
2023-05-12 21:47:40 -0300

    classe filha

---
+++ POO_noite/2º Bimestre/AbstratoFuncionario/Comissionado.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbstratoFuncionario
+{
+    public class Comissionado : Funcionario
+    {
+        public double Porcentagem { get; set; }
+        public Comissionado(int c, string n, double s, double p) : base(cod, nome, salario)
+        {
+            Porcentagem = p;
+        }
+        public override double CalcularSalario(int diasUteis)
+        {
+            return Salario += Salario * 30 * diasUteis * Porcentagem;
+        }
+        public override void Mostrar()
+        {
+            base.Mostrar();
+            System.Console.WriteLine($"Porcentagem: {Porcentagem}");
+        }
+
+    }
+}
