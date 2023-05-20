48d9ea42a8121c3264b3e4486f347c31a75c7526
Martarelli <renanmartarelli@gmail.com>
2023-05-20 13:45:57 -0300

    program

--- POO_noite/2º Bimestre/InterfaceAutenticavel/Program.cs
+++ POO_noite/2º Bimestre/InterfaceAutenticavel/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using InterfaceAutenticavel;
+
+Gerente g = new Gerente();
+g.Nome = "Gerentão";
+g.Senha = 123;
+
+Diretor d = new Diretor();
+d.Nome = "Diretor zica";
+d.Senha = 100;
+
+Cliente c = new Cliente();
+c.Nome = "Clientela";
+c.Endereco = "Rua da aldeia";
+c.Senha = 200;
+
+IAutenticavel ia;
+ia = g;
+System.Console.WriteLine($"{g.Nome} - {ia.Autenticar(123)}"); 
+System.Console.WriteLine($"{d.Nome} - {d.Autenticar(18)}"); 
+ia = c;
+System.Console.WriteLine($"{c.Nome} - {ia.Autenticar(1099)}"); 
+
+
