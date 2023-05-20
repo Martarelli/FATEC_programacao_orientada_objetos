3894787d66bef84383320d6826438ed272390b76
Martarelli <renanmartarelli@gmail.com>
2023-05-20 13:21:57 -0300

    class cliente

---
+++ POO_noite/2º Bimestre/InterfaceAutenticavel/Cliente.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterfaceAutenticavel
+{
+    public class Cliente : IAutenticavel
+    {
+        public string Nome { get; set; } = "";
+        public string Endereco { get; set; } = "";
+        public int Senha { get; set; }
+
+        public bool Autenticar(int s)
+        {
+            return Senha == s;
+        }
+
+    }
+}
