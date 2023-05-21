54b299e6c526f15c672e9b38cb12144e6a050a78
Martarelli <renanmartarelli@gmail.com>
2023-05-21 01:46:48 -0300

    interface tributavel

---
+++ POO_noite/2º Bimestre/ExercicioInterface/ITributavel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExercicioInterface
+{
+    public interface ITributavel
+    {
+        public double CalculaTributos();
+    }
+}
