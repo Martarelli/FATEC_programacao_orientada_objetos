429fa6799dd60626ff63d59644f5976aec189102
Martarelli <renanmartarelli@gmail.com>
2023-05-21 02:02:22 -0300

    instancia dos objetos

--- POO_noite/2º Bimestre/ExercicioInterface/Program.cs
+++ POO_noite/2º Bimestre/ExercicioInterface/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using ExercicioInterface;
+
+ContaCorrente c1 = new ContaCorrente();
+c1.Saldo = 1000;
+
+ContaCorrente c2 = new ContaCorrente();
+c2.Saldo = 2000;
+
+SeguroDeVida s1 = new SeguroDeVida();
+SeguroDeVida s2 = new SeguroDeVida();
+
+TotalizadorDeTributos tt = new TotalizadorDeTributos();
+ITributavel it = c1;
+tt.Adiciona(it);
+ITributavel it = c2;
+tt.Adiciona(it);
+ITributavel it = s1;
+tt.Adiciona(it);
+ITributavel it = s2;
+tt.Adiciona(it);
+
+System.Console.WriteLine($"Total dos tributos: {tt.Total:C}");
+
+
+
