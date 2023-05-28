83cf469064dcbf86547b49ee1313dfc72f69aa40
Martarelli <renanmartarelli@gmail.com>
2023-05-28 14:09:14 -0300

    projeto criado

---
+++ POO_noite/2º Bimestre/ExcecaoArquivo/Program.cs
@@ -0,0 +1,29 @@
+﻿try
+{
+// Isto causará uma exceção
+File.OpenRead("\\teste\\ArquivoLeitura.txt");
+}
+catch (FileNotFoundException e)
+{
+Console.WriteLine();
+Console.WriteLine("Erro pelo FileNotFoundException");
+Console.WriteLine("-------------------------------------");
+Console.WriteLine(e.ToString());
+Console.WriteLine();
+}
+catch (DirectoryNotFoundException e)
+{
+Console.WriteLine();
+Console.WriteLine("Erro pelo DirectoryNotFoundException");
+Console.WriteLine("-------------------------------------");
+Console.WriteLine(e.ToString());
+Console.WriteLine();
+}
+catch (Exception e)
+{
+Console.WriteLine();
+Console.WriteLine("Erro pelo Exception");
+Console.WriteLine("-------------------------------------");
+Console.WriteLine(e.ToString());
+Console.WriteLine();
+}
