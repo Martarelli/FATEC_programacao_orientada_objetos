06c37ee2a33630810c673deab8e5fcb8a8655794
Martarelli <renanmartarelli@gmail.com>
2023-05-28 16:52:09 -0300

    exercicio de excecao finalizado

--- POO_noite/2º Bimestre/ExercicioExcecao/Program.cs
+++ POO_noite/2º Bimestre/ExercicioExcecao/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExercicioExcecao;
+
+Conta c1 = new Conta(1, "Martarelli", 0);
+c1.Depositar(-200);
+
+System.Console.WriteLine("Fim do programa....");
